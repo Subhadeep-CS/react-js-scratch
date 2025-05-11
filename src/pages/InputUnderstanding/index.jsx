3774e5a5dff16e28ee1f629da62aa1935ee8f480
Subhadeep-CS <dassubhadeep631@gmail.com>
2025-05-11 20:25:34 +0530

    11/05

--- src/pages/InputUnderstanding/index.jsx
+++ src/pages/InputUnderstanding/index.jsx
@@ -6,21 +6,93 @@ const InputUndestanding = () => {
    * console.log();
    * })
    */
-  const [inputValue, setInputValue] = useState(0);
-  const handleChange = (e) => {
-    console.log(e.target.value);
-    setInputValue(e.target.value);
-  };
 
-  const handleClick = () => {
-    setInputValue((prevInput) => Number(prevInput) + 1);
+  const [formInput, setFormInputState] = useState({
+    firstName: "",
+    lastName: "",
+  });
+
+  const [errors, setErrors] = useState({ firstName: "", lastName: "" });
+
+  const handleChange = (e) => {
+    //grab the input id
+    const { id, value } = e.target;
+    setFormInputState((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+
+    switch (id) {
+      case "firstName":
+        if (value.length < 6) {
+          setErrors((prevError) => ({
+            ...prevError,
+            [id]: "First Name is too shorts",
+          }));
+        } else {
+          setErrors((prevError) => ({
+            ...prevError,
+            [id]: "",
+          }));
+        }
+        break;
+      case "lastName":
+        if (value.length < 6) {
+          setErrors((prevError) => ({
+            ...prevError,
+            [id]: "lastName is too shorts",
+          }));
+        } else {
+          setErrors((prevError) => ({
+            ...prevError,
+            [id]: "",
+          }));
+        }
+        break;
+      default:
+        "Too many arguments";
+    }
+  };
+  //onChange onKeyDown onKeyUp
+  console.log("Error", errors);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (errors.firstName || errors.lastName) return;
+    if (errors.firstName === "" || errors.lastName === "") return;
+    console.log("api submitted");
   };
   return (
     <div>
-      <input type="number" onChange={handleChange} value={inputValue} />
+      {/* <input type="number" onChange={handleChange} value={inputValue} />
       <button onClick={handleClick}>+</button>
       <br />
       {inputValue}
+      <br />
+      <input type="text" onChange={handleTextChange} />
+      <br />
+      {inputText} */}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="firstName">First Name</label>
+        <input
+          type="text"
+          id="firstName"
+          onChange={handleChange}
+          value={formInput.firstName}
+        />
+        {errors.firstName && <p>{errors.firstName}</p>}
+        <br />
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          type="text"
+          id="lastName"
+          onChange={handleChange}
+          value={formInput.lastName}
+        />
+        {errors.lastName && <p>{errors.lastName}</p>}
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      {formInput.firstName + " " + formInput.lastName}
     </div>
   );
 };
